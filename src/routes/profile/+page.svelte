89472990496c72738916861549478f50853f223f
theayze <fahmimikail86@gmail.com>
2023-01-19 00:24:08 +0800

    makepayment

---
+++ src/routes/profile/+page.svelte
@@ -0,0 +1,67 @@
+<script>
+    import Nav from '../mainnav.svelte';
+</script>
+
+<Nav />
+
+<!-- component -->
+<link rel="stylesheet" href="https://demos.creative-tim.com/notus-js/assets/styles/tailwind.css">
+<link rel="stylesheet" href="https://demos.creative-tim.com/notus-js/assets/vendor/@fortawesome/fontawesome-free/css/all.min.css">
+
+<main class="profile-page">
+    <section class="relative block h-500-px">
+        <div class="absolute top-0 w-full h-full bg-center bg-cover" style="background-image: url('bg.jpg');">
+        <span id="blackOverlay" class="w-full h-full absolute opacity-50 bg-black"></span>
+        </div>
+        <div class="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden h-70-px" style="transform: translateZ(0px)">
+        <svg class="absolute bottom-0 overflow-hidden" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" version="1.1" viewBox="0 0 2560 100" x="0" y="0">
+            <polygon class="text-blueGray-200 fill-current" points="2560 0 2560 100 0 100"></polygon>
+        </svg>
+        </div>
+    </section>
+    <section class="relative py-16 bg-blueGray-200">
+        <div class="container mx-auto px-4">
+        <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg -mt-64">
+            <div class="px-6">
+            <div class="flex flex-wrap justify-center">
+                <div class="w-full lg:w-3/12 px-4 lg:order-2 flex justify-center">
+                </div>
+            </div>
+            <div class="text-center mt-12">
+                <h3 class="text-4xl font-semibold leading-normal mb-2 text-blueGray-700 mb-2">
+                Jenna Stones
+                </h3>
+                <div class="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold uppercase">
+                <i class="fas fa-map-marker-alt mr-2 text-lg text-blueGray-400"></i>
+                Los Angeles, California
+                </div>
+                <div class="mb-2 text-blueGray-600 mt-10">
+                <i class="fas fa-briefcase mr-2 text-lg text-blueGray-400"></i>Solution Manager - Creative Tim Officer
+                </div>
+                <div class="mb-2 text-blueGray-600">
+                <i class="fas fa-university mr-2 text-lg text-blueGray-400"></i>University of Computer Science
+                </div>
+            </div>
+            <div class="mt-10 py-10 border-t border-blueGray-200 text-center">
+                <div class="flex flex-wrap justify-center">
+                <div class="w-full lg:w-9/12 px-4">
+                    <p class="mb-4 text-lg leading-relaxed text-blueGray-700">
+                    An artist of considerable range, Jenna the name taken by
+                    Melbourne-raised, Brooklyn-based Nick Murphy writes,
+                    performs and records all of his own music, giving it a
+                    warm, intimate feel with a solid groove structure. An
+                    artist of considerable range.
+                    </p>
+                    <a href="#pablo" class="font-normal text-pink-500">Show more</a>
+                </div>
+                </div>
+            </div>
+            </div>
+        </div>
+        </div>
+        <footer class="relative bg-blueGray-200 pt-8 pb-6 mt-8">
+    <div class="container mx-auto px-4">
+    </div>
+    </footer>
+    </section>
+</main>
